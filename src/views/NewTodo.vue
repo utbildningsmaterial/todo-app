1dec2a14b091c8674102094b2a2a9531a38e1849
Johan Kivi <johan@kivi.nu>
2019-02-26 11:56:27 +0100

    codelicode

---
+++ src/views/NewTodo.vue
@@ -0,0 +1,73 @@
+<template>
+    <main id="new-todo">
+        <header>
+
+        </header>
+        <section class="content">
+            <input type="text" v-model="newTodo">
+        </section>
+        <footer>
+            <a href="#" class="btn">Add new Todo</a>
+        </footer>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'newTodo',
+    data(){
+        return {
+            newTodo: ''
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+    #new-todo {
+
+        .content {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            input {
+                font-size: 1.4rem;
+                font-weight: 700;
+                border: none;
+                appearance: none;
+                background: none;
+                text-align: center;
+                padding: .25rem;
+
+                &:focus {
+                    outline: none;
+                }
+            }
+
+        }
+
+        footer {
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-end;
+            
+            a.btn {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background: #222;
+                color: white;
+                font-weight: 700;
+                text-decoration: none;
+                height: 3rem;
+                border-radius: 99rem;
+            }
+
+        }
+
+    }
+
+
+</style>
---
+++ src/views/Todos.vue
@@ -0,0 +1,65 @@
+<template>
+    <main id="todos">
+        <header>
+            <h1>TEFLON</h1>
+            <p>När det har svårt att fastna...</p>
+        </header>
+        <section class="content">
+            <todo />
+        </section>
+        <footer>
+            <a href="#" class="btn">Slide to add new</a>
+        </footer>
+    </main>
+</template>
+
+<script>
+
+import todo from '@/components/Todo';
+
+export default {
+    name: 'todos',
+    components: {
+        todo
+    }
+}
+</script>
+
+<style lang="scss">
+
+    #todos {
+
+        header {
+
+            h1 {
+                margin: 0;
+                padding: 0;
+                font-weight: 700;
+            }
+
+            p {
+                margin: 0;
+                font-style: italic;
+            }
+        }
+
+        footer {
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-end;
+            
+            a.btn {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background: #222;
+                color: white;
+                font-weight: 700;
+                text-decoration: none;
+                height: 3rem;
+                border-radius: 99rem;
+            }
+        }
+    }
+
+</style>
